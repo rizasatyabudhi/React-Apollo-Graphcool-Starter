babc4d33f4b1aecf35121a564782d0ff7b8d9064
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-11-28 14:30:55 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+##Starter Repo for React-Apollo-GraphCool
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import "tachyons";
+// import "./index.css";
+
+// __SIMPLE_API_ENDPOINT__ looks like: 'https://api.graph.cool/simple/v1/__SERVICE_ID__'
+const httpLink = new HttpLink({
+  uri: "__SIMPLE_API_ENDPOINT"
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Route exact path="/" component={} />
+      </div>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
